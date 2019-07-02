f8d6ba84818a3a796fd6333dbcc63e23f8024080
Narrakas <netkas@users.noreply.github.com>
2019-07-02 14:58:26 -0400

    Added test for path scope handler

---
+++ tests/linear_usage.php
@@ -0,0 +1,21 @@
+<?php
+
+    use CoffeeHouse\Classes\StringDistance;
+
+    $SourceDirectory = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+    include_once($SourceDirectory . 'CoffeeHouse' . DIRECTORY_SEPARATOR . 'CoffeeHouse.php');
+
+    print("String 1: 'Hello World'\n");
+    print("String 2: 'Hello World'\n");
+    var_dump(StringDistance::levenshtein("Hello World", "Hello World"));
+    print("\n\n");
+
+    print("String 1: 'Hello World'\n");
+    print("String 2: 'hello world'\n");
+    var_dump(StringDistance::levenshtein("Hello World", "hello world"));
+    print("\n\n");
+
+    print("String 1: 'Hello World'\n");
+    print("String 2: 'hello wold'\n");
+    var_dump(StringDistance::levenshtein("Hello World", "hello wold"));
+    print("\n\n");
