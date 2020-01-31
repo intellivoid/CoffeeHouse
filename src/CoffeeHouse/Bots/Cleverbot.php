1102f99b344f25237342121484673a67292706ff
netkas <netkas@intellivoid.info>
2020-01-31 14:16:05 -0300

    Minor performance boost

--- src/CoffeeHouse/Bots/Cleverbot.php
+++ src/CoffeeHouse/Bots/Cleverbot.php
@@ -101,6 +101,7 @@
          * @throws DatabaseException
          * @throws ForeignSessionNotFoundException
          * @throws InvalidSearchMethodException
+         * @noinspection PhpUnused
          */
         public function loadSession(string $session_id)
         {
@@ -176,36 +177,6 @@
             catch(InvalidMessageException $invalidMessageException)
             {
                 // Ignore this exception
-            }
-
-
-            // Telegram Logging Archive
-            try
-            {
-                $Log = "<b>Session ID:</b> <code>" . $this->Session->SessionID . "</code>\n\n";
-                $Log .= "<b>INPUT</b>\n    <i>" . htmlspecialchars($input) . "</i>\n\n";
-                $Log .= "<b>OUTPUT</b>\n    <i>" . htmlspecialchars($Text) . "</i>";
-
-                $url = 'https://api.telegram.org/bot869979136:AAEi_uxDobRLwhC0wF0TMfkqAoy8IC0fA-0/sendMessage';
-                $data = array(
-                    'chat_id' => '-1001184547366',
-                    'parse_mode' => 'html',
-                    'text' => $Log
-                );
-
-                $options = array(
-                    'http' => array(
-                        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                        'method'  => 'POST',
-                        'content' => http_build_query($data)
-                    )
-                );
-                $context  = stream_context_create($options);
-                file_get_contents($url, false, $context);
-            }
-            catch(Exception $exception)
-            {
-                // Ignore this error
             }
 
             return $Text;
