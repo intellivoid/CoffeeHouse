6a746d24d9e8128ae5bdf98f83da1e1be41a81c7
netkas <netkas@intellivoid.info>
2020-03-28 22:04:49 -0300

    Updated structure 'user_subscriptions' to use signed signatures

--- database/coffeehouse_table_user_subscriptions.sql
+++ database/coffeehouse_table_user_subscriptions.sql
@@ -1,15 +1,20 @@
+create table if not exists user_subscriptions
+(
+    id                int auto_increment comment 'Primary unique internal Database ID for this record',
+    account_id        int null comment 'The ID of the user''s Intellivoid Account',
+    subscription_id   int null comment 'The ID of the subscription that this user is associated to',
+    access_record_id  int null comment 'The ID of the access record ID used for the API',
+    status            int null comment 'The status of this user subscription',
+    created_timestamp int null comment 'The Unix Timestamp of when this record was created',
+    constraint user_subscriptions_access_record_id_uindex
+        unique (access_record_id),
+    constraint user_subscriptions_account_id_uindex
+        unique (account_id),
+    constraint user_subscriptions_id_uindex
+        unique (id)
+)
+    comment 'Table of user subscriptions to keep track of the components of the IVA System';
 
--- --------------------------------------------------------
+alter table user_subscriptions
+    add primary key (id);
 
---
--- Table structure for table `user_subscriptions`
---
-
-CREATE TABLE `user_subscriptions` (
-  `id` int(255) NOT NULL COMMENT 'Primary unique internal Database ID for this record',
-  `account_id` int(255) DEFAULT NULL COMMENT 'The ID of the user''s Intellivoid Account',
-  `subscription_id` int(255) DEFAULT NULL COMMENT 'The ID of the subscription that this user is associated to',
-  `access_record_id` int(255) DEFAULT NULL COMMENT 'The ID of the access record ID used for the API',
-  `status` int(255) DEFAULT NULL COMMENT 'The status of this user subscription',
-  `created_timestamp` int(255) DEFAULT NULL COMMENT 'The Unix Timestamp of when this record was created'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table of user subscriptions to keep track of the components of the IVA System';
