57adf812cbf910bfd7af2a81a4e3b7c54094e3fb
netkas <netkas@intellivoid.info>
2020-06-08 23:32:53 -0400

    Updated existing structures

--- database/spam_prediction_cache.sql
+++ database/spam_prediction_cache.sql
@@ -1,4 +1,4 @@
-create table if not exists spam_detection_cache
+create table if not exists spam_prediction_cache
 (
     id               int(255) auto_increment comment 'Unique Internal Database ID',
     hash             varchar(256) null comment 'The hash of the spam contents',
@@ -8,5 +8,5 @@ create table if not exists spam_detection_cache
     created          int(255)     null comment 'The Unix Timestamp of when this cache record was created',
     constraint spam_detection_cache_hash_uindex unique (hash),
     constraint spam_detection_cache_id_uindex unique (id)
-) comment 'The cache table for spam detection';
-alter table spam_detection_cache add primary key (id);
+) comment 'The cache table for spam prediction';
+alter table spam_prediction_cache add primary key (id);
