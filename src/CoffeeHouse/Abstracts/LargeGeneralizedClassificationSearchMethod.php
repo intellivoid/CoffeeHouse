43b55f227d9227c7b305a5533b6342d993bec3b3
netkas <netkas@intellivoid.info>
2020-07-22 19:32:44 -0400

    Added LargeGeneralizedClassificationSearchMethod to \CoffeeHouse\Abstracts

---
+++ src/CoffeeHouse/Abstracts/LargeGeneralizedClassificationSearchMethod.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    namespace CoffeeHouse\Abstracts;
+
+    /**
+     * Class LargeGeneralizedClassificationSearchMethod
+     * @package CoffeeHouse\Abstracts
+     */
+    abstract class LargeGeneralizedClassificationSearchMethod
+    {
+        const byId = "id";
+
+        const byPublicId = "public_id";
+    }
