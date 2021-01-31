d8a1b2669dacb3fdb8ebd1a90dba8d903b483057
Netkas <netkas@intellivoid.info>
2021-01-31 15:12:41 -0500

    Added test 'language_prediction_split_sentence'

---
+++ tests/language_prediction_split_sentence.php
@@ -0,0 +1,31 @@
+<?php
+
+    //$SourceDirectory = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+    //include_once($SourceDirectory . 'CoffeeHouse' . DIRECTORY_SEPARATOR . 'CoffeeHouse.php');
+
+    /** @noinspection PhpIncludeInspection */
+
+    use CoffeeHouse\CoffeeHouse;
+use ppm\ppm;
+
+    require("ppm");
+    ppm::import("net.intellivoid.coffeehouse");
+
+    $CoffeeHouse = new CoffeeHouse();
+
+    function getInput(): string
+    {
+        print(" > ");
+        $handle = fopen ("php://stdin","r");
+        $line = fgets($handle);
+        fclose($handle);
+        print("\n");
+        return $line;
+    }
+
+    while(true)
+    {
+        $Results = $CoffeeHouse->getLanguagePrediction()->predictSentences(getInput());
+        var_dump($Results);
+        print($Results->LanguagePrediction->combineResults()[0]->Language);
+    }
