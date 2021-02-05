ba0d076dbeffbe2fec5871952940dac941908803
Netkas <netkas@intellivoid.info>
2021-02-05 14:18:43 -0500

    Updated package.json

--- tests/translate_test.php
+++ tests/translate_test.php
@@ -7,7 +7,7 @@ require("ppm");
 
     $CoffeeHouse = new CoffeeHouse();
 
-    $input = "Intellivoid has now came to acquire Haruka Aya with the intention to improve it provide it a home on our servers, we trust that we will do a good job in keeping the integrity of Haruka Aya all while improving it to be the best competing Group Manager Bot that Telegram has ever seen. This can only be accomplished with hard work, in the next few days we will begin the process of moving Haruka Aya into Intellivoid's GitHub organization and start to focus on improving the bots performance, community and stability.";
+    $input = "Intellivoid has now came to acquire Haruka Aya with the intention to improve it provide it a home on our servers, we trust that we will do a good job in keeping the integrity of Haruka Aya all while improving it to be the best competing Group Manager Bot that Telegram has ever seen. This can only be accomplished with hard work, in the next few days we will begin the process of moving Haruka Aya into Intellivoid's GitHub organization and start to focus on improving the bots performance, community and stability. HAHAHHA";
 
     $Results = $CoffeeHouse->getTranslator()->translate($input, "español",
         $CoffeeHouse->getLanguagePrediction()->predict($input)->combineResults()[0]->Language
